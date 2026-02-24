d116aebb9f8a8bc38948860dacf4717d0c406ec7
HarshalB84 <harshalbhoyar84@gmail.com>
2026-02-24 23:42:02 +0530

    DSA Day 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import arrays_hashing.BestTimeToBuyAndSellStock;
-import arrays_hashing.ContainsDuplicate;
-import arrays_hashing.TwoSum;
+import arrays_hashing.*;
 import strings.ValidAnagram;
 
 import java.util.Arrays;
@@ -20,11 +18,21 @@ public class Main {
         int [] nums = {1,3,5,7,9,11};
         int target = 18;
         System.out.println(Arrays.toString(TwoSum.twoSum(nums,target)));
-        */
+
 
         int[] prices = {7,1,5,3,6,4};
         System.out.println(BestTimeToBuyAndSellStock.maxProfit(prices));
 
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        System.out.println(Arrays.toString(Merge_Sorted_Array.mergeSortedArray(nums1,m,nums2,n)));
+        */
+
+        int[] nums  = {0,1,0,3,12};
+        MoveZeroesToEnd.movesZeroes(nums);
+        System.out.println(Arrays.toString(nums));
 
     }
 }
---
+++ src/arrays_hashing/MoveZeroesToEnd.java
@@ -0,0 +1,27 @@
+package arrays_hashing;
+
+public class MoveZeroesToEnd {
+
+    /*
+    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero
+    elements.
+    Note that you must do this in-place without making a copy of the array.
+
+    Input: nums = [0,1,0,3,12]
+    Output: [1,3,12,0,0]
+     */
+
+
+    public static void movesZeroes(int[] nums){
+        int count = 0;
+        for(int i=0;i< nums.length;i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[count];
+                nums[count] = temp;
+                count++;
+            }
+        }
+
+    }
+}
